b42a593556ac021dc78d1e661e587004ea17e5cd
kole ervine <koleervine@gmail.com>
2023-03-08 10:10:56 -0700

    adding constraints for rowheight

--- EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
+++ EmojiDictionary/EmojiDictionary/EmojiTableViewController.swift
@@ -35,11 +35,8 @@ class EmojiTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 44.0
     }
     
     
